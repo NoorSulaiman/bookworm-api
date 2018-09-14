deb92bad7307a3c080c758c0affee38dc1fa2455
NoorSulaiman <noor.s.suliman@gmail.com>
2018-09-14 17:02:00 -0400

    adding book mongodb model, book route & update authenticate middleware to include user id in the headers

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-
+import User from '../models/User';
 
 export default (req, res, next) => {
 
@@ -7,14 +7,15 @@ export default (req, res, next) => {
 
     let token
     if (header) token = header.split(" ")[1];
-    console.log(token)
-
     if (token) {
-        jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
             if (err) {
                 res.status(401).json({ errors: "invalid token" })
             } else {
-                next();
+                User.findOne({ email: decoded.email }).then(user => {
+                    req.currentUser = user;
+                    next();
+                })
             }
         })
     } else {
