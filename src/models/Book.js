e2ebd51b22376f8baea4b26afbd65e954fe3f609
NoorSulaiman <noor.s.suliman@gmail.com>
2018-09-20 02:42:32 -0400

    adding fetch, create & delete routes

--- src/models/Book.js
+++ src/models/Book.js
@@ -6,6 +6,7 @@ const schema = new mongoose.Schema({
     authors: { type: String, required: true },
     cover: { type: String, required: true },
     pages: { type: Number, required: true },
+    progress: { type: Number, default: 0 },
     userId: { type: mongoose.Schema.Types.ObjectId, required: true }
 })
 
--- src/routes/books.js
+++ src/routes/books.js
@@ -37,13 +37,38 @@ router.get("/fetchPages", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-    console.log(req.body);
 
     Book.create({ ...req.body.book, userId: req.currentUser._id })
         .then(book => res.json({ book }))
         .catch(err => res.status(400).json({ errors: parseErrors(err.errors) }));
 });
+
+router.post("/progress", (req, res) => {
+
+    Book.findOneAndUpdate(
+
+        { userId: req.currentUser._id, goodreadsId: req.body.data.goodreadsId },
+        { progress: req.body.data.progress },
+        { new: true }
+
+    ).then(book => {
+        if (book) {
+            book.save().then((book) => res.json({ book }))
+        } else { res.status(404).json({ errors: { global: "there is no such a book" } }) }
+    })
+});
+
 router.get("/", (req, res) => {
     Book.find({ userId: req.currentUser._id }).then(books => res.json({ books }));
 });
+
+router.delete("/deleteBook", (req, res) => {
+
+    Book.deleteOne({ userId: req.currentUser._id, goodreadsId: req.body.goodreadsId }).then(book => {
+        if (book.n) {
+            res.status(200).json({ Book: 'Book has been deleted ! ', bookId: req.body.goodreadsId })
+        } else { res.status(404).json({ errors: { global: "there is no such a book" } }) }
+    })
+})
+
 export default router;
